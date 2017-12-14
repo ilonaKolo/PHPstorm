20e9bf2a44616717afa00e64ff9c4c3d0be75514
ilona <ilona.kolossova@khk.ee>
2017-12-14 13:32:27 +0200

    numbrid2

---
+++ numbrid2.php
@@ -0,0 +1,10 @@
+<?php
+for($arv = 1; $arv <= 10; $arv++) {
+    if($arv % 2 == 0) {
+        $varv = 'blue';
+    } else {
+        $varv = 'red';
+    }
+    echo '<font color ="'.$varv.'">'.$arv
+        .'</font><br/ >';
+}
